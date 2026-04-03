954cd4e6d1fb9e97e92dd88535ffa503c4ab64a9
Cecilia Grace <cecigrace2003@gmail.com>
2026-04-03 22:44:51 +0300

    added index.js code(merging the arrays)

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+require('datejs')
+
+function combineUsers(...args) {
+  combinedObject = {
+    users: [],
+    merge_date: ''
+  }
+  
+  combinedObject.merge_date = new Date().toString('M/d/yyyy')
+
+  args.forEach(singleArray => {
+    combinedObject.users.push(...singleArray)
+  })
+  return combinedObject
+}
 
 
 
